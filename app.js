dabda7ec5fdd01174837d81a5526f7a25cd16476
Sarah Weeks <sarahweeks@Sarahs-MacBook-Pro.local>
2020-02-20 10:51:19 +0000

    Convertion function working

--- app.js
+++ app.js
@@ -36,13 +36,18 @@ timerButton.addEventListener("click", () => {
 let teamOneScore =0
 let teamTwoScore =0
 
+document.getElementById("con").style.display = "none";
+document.getElementById("con2").style.display = "none";
+
 tryy.addEventListener("click", () => {
     teamOneScore += 5
     t1s.innerText = teamOneScore
+    document.getElementById("con").style.display = "block";
 })
 try2.addEventListener("click", () => {
     teamTwoScore += 5
     t2s.innerText = teamTwoScore
+    document.getElementById("con2").style.display = "block";
 })
 pen.addEventListener("click", () => {
     teamOneScore += 3
@@ -55,10 +60,12 @@ pen2.addEventListener("click", () => {
 con.addEventListener("click", () => {
     teamOneScore += 2
     t1s.innerText = teamOneScore
+    document.getElementById("con").style.display = "none";
 })
 con2.addEventListener("click", () => {
     teamTwoScore += 2
     t2s.innerText = teamTwoScore
+    document.getElementById("con2").style.display = "none";
 })
 dg.addEventListener("click", () => {
     teamOneScore += 3
